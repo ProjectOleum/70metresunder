30a2dc3984645fd5eaea32c643be583485d7c57c
MarkBrouwer <markbrouwer@dhcp-141-137.cu-wireless-students.carleton.ca>
2017-02-07 16:57:27 -0500

    New design layout including video

--- js/init.js
+++ js/init.js
@@ -5,4 +5,14 @@
     $('.parallax').parallax();
 
   }); // end of document ready
+  
+    $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+
+
+var intro = document.querySelector('.banner');
+var introPlayer = document.querySelector('.banner__video');
+
+    
 })(jQuery); // end of jQuery name space
