1335a2bde9cdd48cb3915a9f01ea14715e682dd8
MarkBrouwer <markbrouwer@dhcp-147-155.cu-wireless-students.carleton.ca>
2017-03-21 15:47:21 -0400

    Update About Section

--- js/init.js
+++ js/init.js
@@ -10,6 +10,26 @@
     $('.scrollspy').scrollSpy();
   });
 
+
+    $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+
+    // Next slide
+$('.carousel').carousel('next');
+$('.carousel').carousel('next', 3); // Move next n times.
+// Previous slide
+$('.carousel').carousel('prev');
+$('.carousel').carousel('prev', 4); // Move prev n times.
+// Set to nth slide
+$('.carousel').carousel('set', 4);
+
+$('.carousel.carousel-slider').carousel({fullWidth: true});
+        
+
+
+
+
 var _interactiveMap = document.getElementById('interactive-map');
 
 var interactiveMap = bodymovin.loadAnimation({
