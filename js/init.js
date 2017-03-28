abce1cd8814142faf29e087d14fc48ace5fcde77
MarkBrouwer <markbrouwer@dhcp-128-174.cu-wireless-students.carleton.ca>
2017-03-28 18:50:00 -0400

    Added modals for each card

--- js/init.js
+++ js/init.js
@@ -304,9 +304,13 @@ $("#switch1").click(function(){
 */
 
 
-
-
-
+/* MODAL STUFF */
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+      
+/* Modal Stuff */
 
 
 
