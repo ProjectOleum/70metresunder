98fa15025d1d463385df6790512e2251c15604cc
MarkBrouwer <markbrouwer@dhcp-128-174.cu-wireless-students.carleton.ca>
2017-03-29 15:01:51 -0400

    Added Audio Clips 2,3,4,5

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 const mySiema = new Siema({
-    perPage: 3,
+
+    //perPage: 3,
+	  perPage: {
+	   768: 2,
+	  1024: 3,
+	},
 });
 
 
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
+
+
+
+
 
 prev.addEventListener('click', () => mySiema.prev());
 next.addEventListener('click', () => mySiema.next());
